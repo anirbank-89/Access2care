72840a5f7005c3feef081d662fb53b699ec67b8d
anirbank-89 <86946524+anirbank-89@users.noreply.github.com>
2021-10-18 14:53:01 +0530

    admin register, login, create home banner api

--- controllers/admin/HomeBanner.js
+++ controllers/admin/HomeBanner.js
@@ -4,7 +4,13 @@ const { Validator } = require('node-input-validator');
 
 const HOME_BANNER = require('../../models/home_banners');
 
-var create = async (req,res)=>{}
+// Create home page banner
+var create = async (req,res)=>{
+    const V = new Validator(req.body, {
+        heading: 'required',
+        description: 'required'
+    });
+}
 
 module.exports = {
     create
